b87d4fb96163da6874b87b4c8a1f60a6c3f7b126
tony <tony.spicer.covin@gmail.com>
2019-10-10 18:53:28 -0500

    Initial commit

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/Tony/PycharmProjects/django_travel_wishlist/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ wishlist/travel_wishlist/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+from .models import Place
+
+admin.site.register(Place)
