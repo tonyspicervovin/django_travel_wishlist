980307704c81217df11dbd6f657b5761abfe3d92
tony <tony.spicer.covin@gmail.com>
2019-10-10 19:59:59 -0500

    added visited page, button and navigation

--- wishlist/travel_wishlist/views.py
+++ wishlist/travel_wishlist/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Place
+from .forms import NewPlaceForm
 
 # Create your views here.
+
+def place_list(request):
+
+    if request.method == 'POST':
+        form = NewPlaceForm(request.POST)
+        place = form.save()
+        if form.is_valid():
+            place.save()
+            return redirect('place_list')
+
+
+    places = Place.objects.filter(visited=False).order_by('name')
+    new_place_form = NewPlaceForm()
+    return render(request, 'travel_wishlist/wishlist.html', { 'places': places, 'new_place_form': new_place_form })
+def places_visited(request):
+    visited = Place.objects.filter(visited=True)
+    return render(request, 'travel_wishlist/visited.html', { 'visited': visited })
+
+def place_was_visited(request):
+    if request.method == 'POST':
+        pk = request.POST.get('pk')
+        place = get_object_or_404(Place, pk=pk)
+        place.visited = True
+        place.save()
+
+    return redirect('place_list')
